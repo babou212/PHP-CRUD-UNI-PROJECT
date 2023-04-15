08e5ee7449afaf1070bccf4d3f881221ff74f587
Dylan Cree <dylanpab1@gmail.com>
2023-04-15 13:00:12 +0100

    added post model, migration and factory. Added basic home blade template

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends Factory<Post>
+ */
+class PostFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+
+        ];
+    }
+}
